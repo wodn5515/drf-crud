ca7383dd95d72965c5ed544610942fbead4633e6
wodn5515 <wodn5515@naver.com>
2021-02-26 21:28:51 +0900

    start project

---
+++ config/settings/development.py
@@ -0,0 +1,9 @@
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '2!m=c%@b8^nre25lnkr&m+ap%_28ua)w646tm&&#lsz=1a4ob!'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
