a9d088bbf12b1b5625ce90aeae13c179605a2e90
wodn5515 <wodn5515@naver.com>
2021-02-27 15:03:20 +0900

    gitignore 수정

--- config/settings/development.py
+++
@@ -1,9 +0,0 @@
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2!m=c%@b8^nre25lnkr&m+ap%_28ua)w646tm&&#lsz=1a4ob!'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
