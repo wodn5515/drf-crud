ae3927f1157f974a1c8e3b40bd88da56dd37b1d6
wodn5515 <wodn5515@naver.com>
2021-03-01 21:57:44 +0900

    Add PostModelViewSet

--- board/views.py
+++ board/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework import viewsets as vs
+from rest_framework import permissions as pm
+from .models import Post
+from .serializers import PostSerializer
+
 
 # Create your views here.
+
+class PostViewSet(vs.ModelViewSet):
+    serializer_class = PostSerializer
+    permission_classes = [pm.IsAuthenticatedOrReadOnly]
+    
+    def get_queryset(self):
+        menu = self.request.GET.get("menu","")
+        if menu:
+            post_list = Post.objects.filter(menu=menu)
+        else:
+            post_list = Post.objects.all()
+        return post_list
+
+    def perform_create(self, serializer):
+        serializer.save(writer=self.request.user)
