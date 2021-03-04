6b344e4b0e6474d6deb33220417dc7efcfc51b29
wodn5515 <wodn5515@naver.com>
2021-03-04 23:37:42 +0900

    Add comment router url

--- board/urls.py
+++ board/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import PostViewSet
+from .views import PostViewSet, CommentViewSet
 
 router = routers.DefaultRouter()
 router.register("post", PostViewSet, basename="post")
+router.register(r"comment/(?P<post>\d+)", CommentViewSet, basename="comment")
 
 urlpatterns = router.urls
