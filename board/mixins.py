6747bfada7db770ecee926c26e39d4bfa95ef29e
wodn5515 <wodn5515@naver.com>
2021-03-05 20:57:09 +0900

    CommentMixin 수정
    RetrieveModelMixin 상속 추가

--- board/mixins.py
+++ board/mixins.py
@@ -3,5 +3,6 @@ from rest_framework import mixins, viewsets
 class CommentMixin(mixins.CreateModelMixin, 
                    mixins.UpdateModelMixin,
                    mixins.DestroyModelMixin,
+                   mixins.RetrieveModelMixin,
                    viewsets.GenericViewSet):
     pass
