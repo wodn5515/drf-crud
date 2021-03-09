56078dab1ddbf7ad81a30087fa74eb6f8642a1f6
wodn5515 <wodn5515@naver.com>
2021-03-09 22:27:12 +0900

    Fix viewsets
    add BoardVierSet
    refactor PostViewset, CommentViewSet

--- backend/board/views.py
+++ backend/board/views.py
@@ -1,27 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
 from rest_framework import viewsets as vs
 from rest_framework import permissions as pm
-from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializers
+
+from .models import Post, Comment, Board
+from .serializers import PostSerializer, CommentSerializers, BoardSerializer
 from .mixins import CommentMixin
+from .permissions import isAdminOrReadOnly
 
 
 # Create your views here.
 
+class BoardViewSet(vs.ModelViewSet):
+    serializer_class = BoardSerializer
+    permission_classes = [isAdminOrReadOnly]
+
+
 class PostViewSet(vs.ModelViewSet):
     serializer_class = PostSerializer
     permission_classes = [pm.IsAuthenticatedOrReadOnly]
+
+    def get_board(self):
+        return get_object_or_404(Board, pk=self.kwargs["board_pk"])
     
     def get_queryset(self):
-        menu = self.request.GET.get("menu", "")
-        if menu:
-            post_list = Post.objects.filter(menu=menu)
-        else:
-            post_list = Post.objects.all()
+        post_list = Post.objects.filter(menu=self.get_board())
         return post_list
 
     def perform_create(self, serializer):
-        serializer.save(writer=self.request.user)
+        serializer.save(writer=self.request.user, board=self.get_board())
 
 
 class CommentViewSet(CommentMixin):
@@ -29,6 +36,10 @@ class CommentViewSet(CommentMixin):
     serializer_class = CommentSerializers
     permission_classes = [pm.IsAuthenticatedOrReadOnly]
 
+    def get_post(self):
+        post = Post.objects.get(pk=self.kwargs["post_pk"])
+        return post
+
     def perform_create(self, serializer):
-        post = Post.objects.get(pk=self.request.GET.get("post", ""))
+        post = Post.objects.get(pk=self.get_post())
         serializer.save(post=post, writer=self.request.user)
