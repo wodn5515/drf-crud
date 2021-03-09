b6961957c58690ccd40eb746d3ca6a5b2514e59e
wodn5515 <wodn5515@naver.com>
2021-03-09 22:26:16 +0900

    Fix api root url

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/board/", include("board.urls"))
+    path("api/", include("board.urls"))
 ]
