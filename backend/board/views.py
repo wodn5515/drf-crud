323fbd9182db1c406f1f26f6ab61b16947fb5c4e
wodn5515 <wodn5515@naver.com>
2021-03-10 11:23:35 +0900

    Fox CommentViewSet

--- backend/board/views.py
+++ backend/board/views.py
@@ -5,7 +5,6 @@ from rest_framework import permissions as pm
 
 from .models import Post, Comment, Board
 from .serializers import PostSerializer, CommentSerializers, BoardSerializer
-from .mixins import CommentMixin
 from .permissions import isAdminOrReadOnly
 
 
@@ -24,22 +23,24 @@ class PostViewSet(vs.ModelViewSet):
         return get_object_or_404(Board, pk=self.kwargs["board_pk"])
     
     def get_queryset(self):
-        post_list = Post.objects.filter(menu=self.get_board())
+        post_list = Post.objects.filter(board=self.get_board())
         return post_list
 
     def perform_create(self, serializer):
         serializer.save(writer=self.request.user, board=self.get_board())
 
 
-class CommentViewSet(CommentMixin):
-    queryset = Comment.objects.all()
+class CommentViewSet(vs.ModelViewSet):
     serializer_class = CommentSerializers
     permission_classes = [pm.IsAuthenticatedOrReadOnly]
 
     def get_post(self):
         post = Post.objects.get(pk=self.kwargs["post_pk"])
         return post
+        
+    def get_queryset(self):
+        comment_list = Comment.objects.filter(post=self.get_post())
+        return comment_list
 
     def perform_create(self, serializer):
-        post = Post.objects.get(pk=self.get_post())
-        serializer.save(post=post, writer=self.request.user)
+        serializer.save(post=self.get_post(), writer=self.request.user)
