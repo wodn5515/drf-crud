4f6141a26fb867c48c861dc6b4364cd110770486
wodn5515 <wodn5515@naver.com>
2021-03-10 11:23:02 +0900

    Remove CommentMixin

--- backend/board/mixins.py
+++ backend/board/mixins.py
@@ -1,8 +1,1 @@
 from rest_framework import mixins, viewsets
-
-class CommentMixin(mixins.CreateModelMixin, 
-                   mixins.UpdateModelMixin,
-                   mixins.DestroyModelMixin,
-                   mixins.RetrieveModelMixin,
-                   viewsets.GenericViewSet):
-    pass
