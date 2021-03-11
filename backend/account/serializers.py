8ebeefe23c7364f4e9ec00639378f121c975e53d
wodn5515 <wodn5515@naver.com>
2021-03-11 21:41:40 +0900

    Add 회원가입 기능

---
+++ backend/account/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers as sz
+
+from .models import User
+
+class RegisterSerializer(sz.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ("username", "nickname", "password")
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
--- backend/account/views.py
+++ backend/account/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
+
+from .serializers import RegisterSerializer
 # Create your views here.
+
+class RegisterView(CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny,]
