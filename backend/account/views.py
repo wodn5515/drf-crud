7fdb4d91e4fac3ed44ab993969d53bd091fe0a88
wodn5515 <wodn5515@naver.com>
2021-03-14 23:09:07 +0900

    Add login, logout

--- backend/account/views.py
+++ backend/account/views.py
@@ -1,7 +1,11 @@
+from django.contrib.auth import authenticate
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.http import require_POST, require_GET
 
 from rest_framework.generics import CreateAPIView
+from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework.authtoken.models import Token
 from rest_framework import status
@@ -22,3 +26,21 @@ class RegisterView(CreateAPIView):
         headers = self.get_success_headers(serializer.data)
         token = Token.objects.create(user=serializer.instance)
         return Response({"token":token.key}, status=status.HTTP_201_CREATED)
+
+
+@method_decorator(require_POST, name="dispatch")
+class LoginView(APIView):
+    def post(self, request):
+        user = authenticate(username=request.data["username"], password=request.data["password"])
+        if user is not None:
+            token, is_create = Token.objects.get_or_create(user=user)
+            return Response({"token": token.key}, status=status.HTTP_201_CREATED)
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+
+@method_decorator(require_GET, name="dispatch")
+class LogoutView(APIView):
+    def get(self, request):
+        user = request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
