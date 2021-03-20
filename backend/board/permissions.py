c1f4c35abe1019be9fbc485249091ebdf6cf9047
wodn5515 <wodn5515@naver.com>
2021-03-20 23:48:02 +0900

    Add IsOwnerOrReadOnly permission

--- backend/board/permissions.py
+++ backend/board/permissions.py
@@ -1,7 +1,7 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
-class isAdminOrReadOnly(BasePermission):
+class IsAdminOrReadOnly(BasePermission):
     """
     읽기권한은 모두, 나머지는 관리자만
     """
@@ -10,4 +10,20 @@ class isAdminOrReadOnly(BasePermission):
             request.method in SAFE_METHODS or
             request.user and
             request.user.is_staff
+        )
+
+class IsOwnerOrReadOnly(BasePermission):
+    """
+    오브젝트 권한은 작성자에게, 나머진 모두
+    """
+    def has_permission(self, request, view):
+        return bool(
+            request.method in SAFE_METHODS or
+            request.user and
+            request.user_authenticated
+        )
+
+    def has_object_permission(self, request, view, obj):
+        return bool(
+            request.user==obj.writer
         )
