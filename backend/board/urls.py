6179acdc47883caf1fb4846a9461be705bf3e5d8
wodn5515 <wodn5515@naver.com>
2021-03-26 20:38:43 +0900

    Fix urlpatterns
    comment관련 url 통합

--- backend/board/urls.py
+++ backend/board/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import PostViewSet, CommentViewSet, BoardViewSet, SubCommentCreateView
+from .views import PostViewSet, CommentViewSet, BoardViewSet
 
 router = routers.DefaultRouter()
 router.register(r"boards", BoardViewSet, basename="board")
@@ -8,7 +8,6 @@ router.register(r"boards/(?P<board_pk>\d+)/posts", PostViewSet, basename="post")
 router.register(r"boards/(?P<board_pk>\d+)/posts/(?P<post_pk>\d+)/comments", CommentViewSet, basename="comment")
 
 urlpatterns = [
-    path("boards/<int:board_pk>/posts/<int:post_pk>/comments/<int:comment_pk>/reply/", SubCommentCreateView.as_view())
 ]
 
 
