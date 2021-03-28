682176dd97c9dbbc0475f1be7347f51ee2b23aa9
wodn5515 <wodn5515@naver.com>
2021-03-28 20:47:46 +0900

    Fix test_user_create using Client

--- backend/account/tests/tests.py
+++ backend/account/tests/tests.py
@@ -1,13 +1,22 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 from ..models import User
 
+import json
+
 # Create your tests here.
+
+client = Client()
 
 class AccountTestCase(TestCase):
     def setUp(self):
         User.objects.create_user(username="testcase", nickname="testcaseX", password="testcase")
 
     def test_user_create(self):
-        test_user = User.objects.get(username="testcase")
-        self.assertEqual(test_user.nickname, "testcase")
+        data = {
+            "username": "unittest",
+            "nickname": "유닛테스트",
+            "password": "dms0300!"
+        }
+        response = client.post("/account/register/", json.dumps(data), content_type="application/json")
+        self.assertEqual(response.status_code, 201)
